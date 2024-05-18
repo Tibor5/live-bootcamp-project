b5abe7a161113f04a162adf9dc245260f42e8b54
Tibor5 <tibibibi.novakovic9@gmail.com>
2024-05-18 19:12:43 +0200

    Sprint 2 (Task 1): feat(routes): refactor out route methods to a module.

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -1,6 +1,9 @@
-use axum::{http::StatusCode, response::IntoResponse, routing::post, serve::Serve, Router};
+use crate::routes::*;
+use axum::{routing::post, serve::Serve, Router};
 use std::error::Error;
 use tower_http::services::ServeDir;
+
+pub mod routes;
 
 pub struct Application {
     server: Serve<Router, Router>,
@@ -26,24 +29,4 @@ impl Application {
         println!("listening on {}", &self.address);
         self.server.await
     }
-}
-
-async fn signup() -> impl IntoResponse {
-    StatusCode::OK.into_response()
-}
-
-async fn login() -> impl IntoResponse {
-    StatusCode::OK.into_response()
-}
-
-async fn logout() -> impl IntoResponse {
-    StatusCode::OK.into_response()
-}
-
-async fn verify_2fa() -> impl IntoResponse {
-    StatusCode::OK.into_response()
-}
-
-async fn verify_token() -> impl IntoResponse {
-    StatusCode::OK.into_response()
 }
